0048ef635e007a376a89c6631a2c2c4991eac78a
frullah <frullah12@gmail.com>
2019-06-13 23:18:55 +0700

    bench: use the code according to its purpose

--- benchmark/func_test.go
+++ benchmark/func_test.go
@@ -4,37 +4,30 @@ import (
 	"testing"
 )
 
-func Fn1(a, b string) string {
-	return a + b
-}
-
-func Fn2(a, b string) string {
-	return b + a
-}
+type EmptyStruct struct{}
 
 func BenchmarkCallClosure(b *testing.B) {
-	var closure func(url, handler string) string
-	arg1 := "arg1 value"
-	arg2 := "arg2 value"
+	closure := noop
 	for i := 0; i < b.N; i++ {
-		if i%2 == 0 {
-			closure = Fn1
-		} else {
-			closure = Fn2
-		}
-
-		closure(arg1, arg2)
+		closure()
 	}
 }
 
 func BenchmarkCallFunction(b *testing.B) {
-	arg1 := "arg1 value"
-	arg2 := "arg2 value"
 	for i := 0; i < b.N; i++ {
-		if i%2 == 0 {
-			Fn1(arg1, arg2)
-		} else {
-			Fn2(arg1, arg2)
-		}
+		noop()
 	}
+}
+
+func BenchmarkSpreadOperator(b *testing.B) {
+	slices := []EmptyStruct{{}, {}, {}, {}, {}, {}}
+	for i := 0; i < b.N; i++ {
+		fnWithVariadicParameter(slices...)
+	}
+}
+
+func noop() {
+}
+
+func fnWithVariadicParameter(values ...EmptyStruct) {
 }
